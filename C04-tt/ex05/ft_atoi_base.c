04a053d64639d5b40c18484b0c85c3a55e51f8dc
Joel Perpetua <qwerty.perpetua@gmail.com>
2022-07-17 20:27:06 +0200

    got first and last usable index, have to do the convertion to dec and return the value

--- C04-tt/ex05/ft_atoi_base.c
+++ C04-tt/ex05/ft_atoi_base.c
@@ -37,19 +37,82 @@ int	ft_validate(char *base, unsigned int *base_int)
 	return (1);
 }
 
+int	ft_found(char c, char *base)
+{
+	int	i;
+	
+	i = 0;
+	while (base[i])
+	{
+		if (c == base[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_get_last(char *str, char *base)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '-' && str[i] != '+' && str[i] != ' ' 
+			&& ft_found(str[i], base) == 0)
+			return (i - 1);
+		if (str[i + 1] == '\0')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_check_char(char *str, char *base, int *negative, int *quantity)
+{
+	int	i;
+	int	first_num;
+
+	i = 0;
+	first_num = 0;
+	while (i < ft_get_last(str, base))
+	{
+		if (str[i] == '-')
+			*negative += 1;
+		if (str[i] != '-' && str[i] != '+' && str[i] != ' ')
+		{
+			if (*quantity == 0 && str[i] != '0')
+			{
+				first_num = i;
+				*quantity += 1;
+			}
+			else if (*quantity != 0)
+				*quantity += 1;
+		}
+		i++;
+	}
+	return (first_num);
+}
+
 int	ft_atoi_base(char *str, char *base)
 {
 	unsigned int	base_int;
+	int	negative;
+	int	quantity;
+	int	first_num;
 	
 	base_int = 0;
-	if (ft_validate(base, base_int) == 0)
+	if (ft_validate(base, &base_int) == 0)
 		return (0);
-	
+	first_num = ft_check_char(str, base, &negative, &quantity);
+	printf("Index of first valid char: %d (%c)\n", first_num, str[first_num]);
+	printf("Index of last valid char: %d (%c)\n", ft_get_last(str, base), str[ft_get_last(str, base)]);
+	return (1);
 }
 
 int	main(void)
 {
 	int	res;
-
 	res = ft_atoi_base("  ---+-89BA", "0123456789ABCDEF");
+	return (res);
 }
