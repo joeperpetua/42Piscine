05b058be947c2642a976caec477f171724546182
Joel Perpetua <qwerty.perpetua@gmail.com>
2022-07-19 16:01:34 +0200

    Adding ex06 and ex07

---
+++ C05-tt/ex07/ft_find_next_prime.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperpetu <jperpetu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 15:39:19 by jperpetu          #+#    #+#             */
+/*   Updated: 2022/07/19 16:00:37 by jperpetu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	i = 2;
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (ft_is_prime(nb) == 0)
+	{
+		nb++;
+	}
+	return (nb);
+}
+
+/*int	main(void)
+{
+	printf("%d\n", ft_find_next_prime(-21));
+	return (0);
+}*/
